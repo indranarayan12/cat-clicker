6491256daa73e57d35bb696a21df470a7ac17ca9
Indra Narayan Sah <36330980+indranarayan12@users.noreply.github.com>
2019-03-19 14:41:09 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const catImg = document.querySelector('img');
-console.log('got it');
-catImg.addEventListener('click',function(){
-    const pTag = document.querySelector('p');
-    var x = pTag.textContent;
-    pTag.textContent = x+1;
-});
